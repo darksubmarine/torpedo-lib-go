0d5b8548f9b58f7ba3901917418fd4d3d24109e1
Sebastian Arrubia <sarrubia@gmail.com>
2024-02-07 17:30:19 -0300

    Improving comments

--- app/provider.go
+++ app/provider.go
@@ -2,6 +2,7 @@ package app
 
 var nilFn = func() error { return nil }
 
+// IProvider dependency provider interface that cover the app life-cycle events
 type IProvider interface {
 	Provide(lf IContainer) (interface{}, error)
 	OnRegister() func() error
@@ -9,8 +10,14 @@ type IProvider interface {
 	OnStop() func() error
 }
 
+// BaseProvider no operation provider to create your own provider
 type BaseProvider struct{}
 
+// OnRegister returns a hook function to be executed at register step
 func (p *BaseProvider) OnRegister() func() error { return nilFn }
-func (p *BaseProvider) OnStart() func() error    { return nilFn }
-func (p *BaseProvider) OnStop() func() error     { return nilFn }
+
+// OnStart returns a hook function to be executed at start step
+func (p *BaseProvider) OnStart() func() error { return nilFn }
+
+// OnStop returns a hook function to be executed at stop step
+func (p *BaseProvider) OnStop() func() error { return nilFn }
