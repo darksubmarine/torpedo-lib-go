8964d7c7185c9f7e51018ae576d315ff19f52d84
Sebastian Arrubia <sarrubia@gmail.com>
2024-05-02 15:27:37 -0300

    Adding Data Context within request gin.Context

---
+++ http/gin_utils/datacontext_middleware.go
@@ -0,0 +1,39 @@
+package gin_utils
+
+import (
+	"github.com/darksubmarine/torpedo-lib-go/context"
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	KeyDataContext = "TPDO_DATA_CONTEXT"
+)
+
+// WithDataContext sets a context.DataMap as context application useful to share data within request execution flow.
+func WithDataContext() func(*gin.Context) {
+	return func(c *gin.Context) {
+		// Setting context.DataMap as context application
+		c.Set(KeyDataContext, context.NewDataMap())
+
+		c.Next()
+	}
+}
+
+// SetDataContext sets a key-value pair into the DataContext map.
+func SetDataContext(c *gin.Context, key string, val interface{}) {
+	dCtx, ok := c.Get(KeyDataContext)
+	if ok {
+		dCtx.(*context.DataMap).Set(key, val)
+		c.Set(KeyDataContext, dCtx)
+	}
+}
+
+// GetDataContext returns the context.DataMap and a boolean to check if it exists.
+func GetDataContext(c *gin.Context) (*context.DataMap, bool) {
+	if dCtx, ok := c.Get(KeyDataContext); ok {
+		cast, ok := dCtx.(*context.DataMap)
+		return cast, ok
+	}
+
+	return nil, false
+}
